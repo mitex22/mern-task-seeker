ef0e89e13c13f6058911d7600b347ecf88505251
mitex22 <mitex22@abv.bg>
2025-09-11 18:39:09 +0300

    db config and connect

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import goalRoutes from './routes/goalRoutes.js';
 import errorHandler from './middleware/errorMiddleware.js';
+import colors from 'colors';
+import { connectDB } from './config/db.js';
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -16,5 +18,7 @@ app.use('/api/goals', goalRoutes);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`.green);
+
+    connectDB();
 });
